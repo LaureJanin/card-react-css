28c0511c7a3bf2afb8a6f67ae48b91abf742eed8
LAURE JANIN <ljanin51@gmail.com>
2022-11-11 16:35:20 +0100

    <message>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import style from 'styled-components';
+import parfum from './images/parfum.jpg';
+
+const Card = style.div`
+  margin-top: 60px;
+  margin-left: 130px;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 75%;
+  padding: 50px;
+  color: #444;
+  background-color: hsl(30, 38%, 92%);
+`;
+
+const Description = style.div`
+  margin-left: 50px;
+`;
+
+const Title = style.h1`
+  font-family: arial,sans-serif;
+  color: #0d1a26;
+  font-weight: 400;
+`;
+
+const Subtitle = style.h2`
+  font-family: arial,sans-serif;
+  font-size: 14px;
+  color: #0d1a26;
+  font-weight: 200;
+`;
+
+const Navigation = style.ul`
+  font-family: courier,sans-serif;
+`;
+
+const Paragraphe = style.p`
+  color: red;
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <Card>
+        <img src={parfum} alt="photo du parfum" />
+        <Description>
+        <Title>Gabrielle Essence</Title>
+        <Subtitle>EAU DE PARFUM</Subtitle>
+        <Navigation>
+          <li>A floral, solar and voluptuous interpretation</li>
+          <li>Composed by Olivier Polge</li>
+          <li>$149.99</li>
+        </Navigation>
+        <Paragraphe>En rupture de stock</Paragraphe>
+        </Description>   
+    </Card>
   );
 }
 
